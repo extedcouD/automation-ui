9c0bdfa25baf7b401310900402c3e81da0fda822
extedcouD <extedcoud@gmail.com>
2024-12-17 15:14:17 +0530

    toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import MainContent from "./components/main-content";
 import TopBar from "./components/top-bar";
@@ -9,6 +10,18 @@ function App() {
 			<main className="pt-16 h-full flex">
 				<MainContent />
 			</main>
+			<ToastContainer
+				position="bottom-right"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss={false}
+				draggable
+				pauseOnHover={false}
+				theme="colored"
+			/>
 		</>
 	);
 }
